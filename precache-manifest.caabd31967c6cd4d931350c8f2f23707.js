6f352f2b9b4ac06d5c8b0805d06cd69315f3731d
Paul156551 <pavel.ryzhkov.99@mail.ru>
2023-07-01 14:05:30 +0300

    Updates

---
+++ precache-manifest.caabd31967c6cd4d931350c8f2f23707.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec76f273d4185c86faace5599825cc42",
+    "url": "/wbreklama/index.html"
+  },
+  {
+    "revision": "f85da5289d85884d9219",
+    "url": "/wbreklama/static/css/main.0307f654.chunk.css"
+  },
+  {
+    "revision": "33636dadceec5f7cd6dd",
+    "url": "/wbreklama/static/js/2.83ad1242.chunk.js"
+  },
+  {
+    "revision": "9f1077c2bf9ddd7a7f8e956bfb9a3985",
+    "url": "/wbreklama/static/js/2.83ad1242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85da5289d85884d9219",
+    "url": "/wbreklama/static/js/main.ad4feb43.chunk.js"
+  },
+  {
+    "revision": "9f68b2f396639531f742",
+    "url": "/wbreklama/static/js/runtime-main.f68592dd.js"
+  },
+  {
+    "revision": "edfa71c34ad47b319702bd4f443c7471",
+    "url": "/wbreklama/static/media/avatar0.edfa71c3.png"
+  },
+  {
+    "revision": "dcc81d2754efc7ccb32579621c1e4436",
+    "url": "/wbreklama/static/media/favicon.dcc81d27.ico"
+  }
+]);
